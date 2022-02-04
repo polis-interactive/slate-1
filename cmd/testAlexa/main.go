281ce5d8ee45076e8d7de0c4c647b09aea738425
Bruce Wilcoxon <brucewilcoxon@twigi-bi.io>
2022-02-04 17:47:18 -0600

    aghhh

--- cmd/testAlexa/main.go
+++ cmd/testAlexa/main.go
@@ -3,7 +3,7 @@ package main
 import alexa "github.com/mikeflynn/go-alexa/skillserver"
 
 var Applications = map[string]interface{}{
-	"/slate-1/LightIntent": alexa.EchoApplication{ // Route
+	"/slate-1/": alexa.EchoApplication{ // Route
 		AppID:    "amzn1.ask.skill.69a5128a-d6b6-4bd2-888d-f388e8986c7b", // Echo App ID from Amazon Dashboard
 		OnIntent: EchoIntentHandler,
 		OnLaunch: EchoIntentHandler,
