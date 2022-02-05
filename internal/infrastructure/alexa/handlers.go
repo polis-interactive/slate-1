a0c7387892276732eaa19fce378cf8d0c2b2da6d
Bruce Wilcoxon <brucewilcoxon@twigi-bi.io>
2022-02-05 15:36:29 -0600

    pretty sure this should work

--- internal/infrastructure/alexa/handlers.go
+++ internal/infrastructure/alexa/handlers.go
@@ -1,10 +1,8 @@
 package alexa
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -15,10 +13,6 @@ type handler struct {
 }
 
 func (h *handler) handleSlateOne(c *gin.Context) {
-
-	b, _ := ioutil.ReadAll(c.Request.Body)
-	println(string(b))
-	c.Request.Body = ioutil.NopCloser(bytes.NewReader(b))
 
 	var body alexaBody
 	if err := c.ShouldBindJSON(&body); err != nil {
